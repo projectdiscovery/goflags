b29f30b86628dc633d213f138955f6e3377004b4
mzack <marco.rivoli.nvh@gmail.com>
2021-07-28 17:23:29 +0200

    misc

--- goflags.go
+++ goflags.go
@@ -324,7 +324,7 @@ func (flagSet *FlagSet) IntVar(field *int, long string, defaultValue int, usage 
 // StringSliceVarP adds a string slice flag with a shortname and longname
 func (flagSet *FlagSet) StringSliceVarP(field *StringSlice, long, short string, defaultValue []string, usage string) *FlagData {
 	for _, item := range defaultValue {
-		_ = field.SetToLower(item)
+		_ = field.Set(item)
 	}
 
 	flag.Var(field, short, usage)
@@ -344,7 +344,7 @@ func (flagSet *FlagSet) StringSliceVarP(field *StringSlice, long, short string, 
 // StringSliceVar adds a string slice flag with a longname
 func (flagSet *FlagSet) StringSliceVar(field *StringSlice, long string, defaultValue []string, usage string) *FlagData {
 	for _, item := range defaultValue {
-		_ = field.SetToLower(item)
+		_ = field.Set(item)
 	}
 
 	flag.Var(field, long, usage)
