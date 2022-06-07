57a3661b8a76cb8d20c47e1db8a8975dae411624
mzack <marco.rivoli.nvh@gmail.com>
2022-06-07 06:55:10 +0200

    improving option comments

--- string_slice_options.go
+++ string_slice_options.go
@@ -1,38 +1,64 @@
 package goflags
 
+// StringSliceOptions represents the default string slice (list of items)
+// Tokenization: None
+// Normalization: None
+// Type: []string
+// Example: -flag value1 -flag value2 => {"value1", "value2"}
 var StringSliceOptions = Options{}
 
-// No value normalization is happening.
+// CommaSeparatedStringSliceOptions represents a list of comma separated items
+// Tokenization: Comma
+// Normalization: None
+// Type: []string
+// Example: -flag value1,value2 => {"value1", "value2"}
 var CommaSeparatedStringSliceOptions = Options{
 	IsEmpty: isEmpty,
 }
 
-// No value normalization is happening.
+// FileCommaSeparatedStringSliceOptions represents a list of comma separated files containing items
+// Tokenization: Comma
+// Normalization: None
+// Type: []string
+// Example: -flag test.txt # with test.txt containing on multiple lines value1 and nvalue2 => {"value1", "value2"}
 var FileCommaSeparatedStringSliceOptions = Options{
 	IsEmpty:    isEmpty,
 	IsFromFile: func(s string) bool { return true },
 }
 
+// NormalizedOriginalStringSliceOptions represents a list of items
+// Tokenization: None
+// Normalization: Standard
+// Type: []string
+// Example: -flag /value/1 -flag value2 => {"/value/1", "value2"}
 var NormalizedOriginalStringSliceOptions = Options{
 	IsEmpty:   isEmpty,
 	Normalize: normalize,
 }
 
-// NormalizedStringSliceVarP adds a path slice flag with a shortname and longname.
-// It supports comma separated values, that are normalized
-// (lower-cased, stripped of any leading and trailing whitespaces and quotes)
+// FileNormalizedStringSliceOptions represents a list of path items
+// Tokenization: Comma
+// Normalization: Standard
+// Type: []string
+// Example: -flag /value/1 -flag value2 => {"/value/1", "value2"}
 var FileNormalizedStringSliceOptions = Options{
 	IsEmpty:    isEmpty,
 	Normalize:  normalizeLowercase,
 	IsFromFile: func(s string) bool { return true },
 }
 
+// FileStringSliceOptions represents a list of items stored in a file
+// Tokenization: Standard
+// Normalization: Standard
 var FileStringSliceOptions = Options{
 	IsEmpty:    isEmpty,
 	Normalize:  normalizeTrailingParts,
 	IsFromFile: func(s string) bool { return true },
 }
 
+// NormalizedStringSliceOptions represents a list of items
+// Tokenization: Comma
+// Normalization: Standard
 var NormalizedStringSliceOptions = Options{
 	IsEmpty:   isEmpty,
 	Normalize: normalizeLowercase,
