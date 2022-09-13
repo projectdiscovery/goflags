a6df981a8ff42ab0211934a4252b726d24ff45fe
Sajad Parra <parrasajad@gmail.com>
2022-09-13 15:06:11 +0530

    add single flag help

--- goflags.go
+++ goflags.go
@@ -501,6 +501,11 @@ func (flagSet *FlagSet) usageFunc() {
 			flagSet.displayGroupUsageFunc(newUniqueDeduper(), group, cliOutput, writer)
 			return
 		}
+		flag := flagSet.getFlagByName(strings.ToLower(os.Args[2]))
+		if flag != nil {
+			flagSet.displaySingleFlagUsageFunc(os.Args[2], flag, cliOutput, writer)
+			return
+		}
 	}
 
 	if len(flagSet.groups) > 0 {
@@ -517,6 +522,17 @@ func (flagSet *FlagSet) getGroupbyName(name string) groupData {
 		}
 	}
 	return groupData{}
+}
+
+func (flagSet *FlagSet) getFlagByName(name string) *FlagData {
+	var flagData *FlagData
+	flagSet.flagKeys.forEach(func(key string, data *FlagData) {
+		if strings.EqualFold(data.long, name) || strings.EqualFold(data.short, name) {
+			flagData = data
+			return
+		}
+	})
+	return flagData
 }
 
 // usageFuncInternal prints usage for command line flags
@@ -583,6 +599,15 @@ func (flagSet *FlagSet) displayGroupUsageFunc(uniqueDeduper *uniqueDeduper, grou
 	writer.Flush()
 	fmt.Printf("\n")
 	return otherOptions
+}
+
+// displaySingleFlagUsageFunc displays usage for a single flag
+func (flagSet *FlagSet) displaySingleFlagUsageFunc(name string, data *FlagData, cliOutput io.Writer, writer *tabwriter.Writer) {
+	if currentFlag := flagSet.CommandLine.Lookup(name); currentFlag != nil {
+		result := createUsageString(data, currentFlag)
+		fmt.Fprint(writer, result, "\n")
+		writer.Flush()
+	}
 }
 
 type uniqueDeduper struct {
